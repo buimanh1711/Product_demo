762305fc84cce597f8f895d43bd04eb0f55baeaa
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 02:37:07 +0700

    manh

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,12 +2,14 @@ const axios = require('axios')
 
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'https://manhbui-testdeploy-mblog.herokuapp.com'
+
 const api = (method, url, data, responseType) => {
   if(method && url) {
     return axios({
       method,
       url,
       data: data || {},
+      headers: {'Content-Type': 'application/json'}
     })
   } else {
     console.log('error somt')
