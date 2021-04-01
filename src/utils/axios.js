2cc555c3d3f0cad03fd8950c0f73572971e9188b
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 17:56:31 +0700

    manh

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,8 @@
 const axios = require('axios')
 
 const testDeploy = axios.create({
-  baseURL: 'https://manhbui-testdeploy-mblog.herokuapp.com',
-  // baseURL: 'http://localhost:5000',
-
   withCredentials: true,
+  baseURL: 'https://manhbui-testdeploy-mblog.herokuapp.com',
 })
 
 const api = (method, url, data) => {
